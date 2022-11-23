c740b37354739354370f5a42a7c52630d44eb682
Chirag Pareek <chirag.pareek@justice.gov.uk>
2022-11-23 13:38:19 +0000

    removed unnecessary code

--- 013-disk-encryption-ADE.tf
+++ 013-disk-encryption-ADE.tf
@@ -28,29 +28,3 @@ resource "azurerm_virtual_machine_extension" "vmextension" {
 
   depends_on = [azurerm_windows_virtual_machine.winvm, azurerm_linux_virtual_machine.linvm, azurerm_virtual_machine_data_disk_attachment.data_disk_attachments]
 }
-
-# resource "azurerm_virtual_machine_extension" "vmextensionlinux" {
-#   count                      = lower(var.vm_type) == "linux" && var.encrypt_ADE ? 1 : 0
-#   name                       = "azure-disk-encrypt-${random_string.password.result}"
-#   location                   = "${data.azurerm_resource_group.test.location}"
-#   resource_group_name        = "${data.azurerm_resource_group.test.name}"
-#   virtual_machine_name       = "${var.vm_name}"
-#   publisher                  = "Microsoft.Azure.Security"
-#   type                       = "AzureDiskEncryptionForLinux"
-#   type_handler_version       = "${var.type_handler_version == "" ? "1.1" : var.type_handler_version}"
-#   auto_upgrade_minor_version = true
-
-#   settings = <<SETTINGS
-#     {
-#         "EncryptionOperation": "${var.encrypt_operation}",
-#         "KeyVaultURL": "${data.azurerm_key_vault.keyvault.vault_uri}",
-#         "KeyVaultResourceId": "${data.azurerm_key_vault.keyvault.id}",					
-#         "KeyEncryptionKeyURL": "${var.encryption_key_url}",
-#         "KekVaultResourceId": "${data.azurerm_key_vault.keyvault.id}",					
-#         "KeyEncryptionAlgorithm": "${var.encryption_algorithm}",
-#         "VolumeType": "${var.volume_type}"
-#     }
-# SETTINGS
-
-#   tags = "${var.tags}"
-# }
