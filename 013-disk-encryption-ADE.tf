a55744bee9ca3854154abde71c8e7ddd471b979b
Chirag Pareek <chirag.pareek@justice.gov.uk>
2022-11-23 10:04:12 +0000

    added ADE encryption

---
+++ 013-disk-encryption-ADE.tf
@@ -0,0 +1,12 @@
+module "diskencrypt" {
+  count                = var.encrypt_ADE ? 1 : 0
+  source               = "Azure/diskencrypt/azurerm"
+  resource_group_name  = var.vm_resource_group
+  location             = var.vm_location
+  vm_name              = var.vm_name
+  key_vault_name       = var.kv_name
+  encryption_algorithm = "RSA-OAEP"
+  encryption_key_url   = azurerm_key_vault_key.disk_enc_key.id
+
+  tags = var.tags
+}
