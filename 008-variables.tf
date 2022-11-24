6a0aac0e52a6838adc58cdc22acde46ac493a26d
Chirag Pareek <chirag.pareek@justice.gov.uk>
2022-11-24 09:48:02 +0000

    removed validation

--- 008-variables.tf
+++ 008-variables.tf
@@ -176,10 +176,6 @@ variable "rc_os_sku" {
 variable "encrypt_CMK" {
   type    = bool
   default = false
-  validation {
-    condition     = encrypt_CMK && encrypt_ADE
-    error_message = "You can NOT encrypt disks with CMK and ADE both, you can only encrypt either CMK or ADE"
-  }
 }
 variable "kv_name" {
   type    = string
@@ -193,10 +189,6 @@ variable "kv_rg_name" {
 variable "encrypt_ADE" {
   type    = bool
   default = false
-  validation {
-    condition     = encrypt_CMK && encrypt_ADE
-    error_message = "You can NOT encrypt disks with CMK and ADE both, you can only encrypt either CMK or ADE"
-  }
 }
 
 ##########
