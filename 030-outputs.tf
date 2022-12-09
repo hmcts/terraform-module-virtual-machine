72687fef753b0fa57f1942f3d54b6bda75bfbb58
Alex Bance <Alex.Bance@justice.gov.uk>
2022-12-09 19:40:15 +0000

    fix: error outputting vm id

--- 030-outputs.tf
+++ 030-outputs.tf
@@ -1,3 +1,3 @@
 output "vm_id" {
-  value = var.vm_type == "windows" ? azurerm_windows_virtual_machine.winvm.id : var.vm_type == "linux" ? azurerm_linux_virtual_machine.linvm.id : null
+  value = var.vm_type == "windows" ? azurerm_windows_virtual_machine.winvm[0].id : var.vm_type == "linux" ? azurerm_linux_virtual_machine.linvm[0].id : null
 }
