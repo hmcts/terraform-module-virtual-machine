b26d8a72694e2502a4099bb5a670dacb261cabee
BRENDON HIGGINS <Brendon.Higgins@hmcts.net>
2022-12-09 17:46:54 +0000

    Fix-typo

--- 030-outputs.tf
+++ 030-outputs.tf
@@ -1,3 +1,3 @@
 output "vm_id" {
-  value = var.vm_type == "windows" ? azurerm_windows_virtual_machine.winvm.[count.index].id : var.vm_type == "linux" ? azurerm_linux_virtual_machine.linvm.[count.index].id : null
+  value = var.vm_type == "windows" ? azurerm_windows_virtual_machine.winvm[count.index].id : var.vm_type == "linux" ? azurerm_linux_virtual_machine.linvm[count.index].id : null
 }
