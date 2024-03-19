88567fc49adb758c9a9dfa5a1ef06ce50a0bfdfa
Connor O'Kane <connor.o'kane@justice.gov.uk>
2024-03-19 12:13:45 +0000

    adding the tagging module

--- interpolated-defaults.tf
+++ interpolated-defaults.tf
@@ -5,16 +5,9 @@ locals {
 
 data "null_data_source" "tag_defaults" {
   inputs = {
-    Project_Name         = var.tag_project_name
-    Environment          = var.tag_environment
-    Cost_Center          = var.tag_cost_center
-    Service              = var.tag_service
-    App_Operations_Owner = var.tag_app_operations_owner
-    System_Owner         = var.tag_system_owner
-    Budget_Owner         = var.tag_budget_owner
-    Created_By           = "Terraform"
-    BuiltFrom            = "hmcts/martha-infra"
-    BusinessArea         = "Cross-Cutting"
-    Application          = "martha"
+    Created_By   = "Terraform"
+    BuiltFrom    = "hmcts/martha-infra"
+    BusinessArea = "Cross-Cutting"
+    Application  = "martha"
   }
 }
