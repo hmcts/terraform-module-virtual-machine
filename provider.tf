ed0852bd436f1d3109e3e266565aa6eed66e353a
Connor O'Kane <connor.o'kane@justice.gov.uk>
2024-03-19 13:00:58 +0000

    adding the tagging module

--- provider.tf
+++ provider.tf
@@ -10,5 +10,6 @@ terraform {
 module "ctags" {
   source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
   environment = lower(var.env)
+  product     = var.product
   builtFrom   = var.builtFrom
 }
