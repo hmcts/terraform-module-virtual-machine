592a991b7a89c0798ff73ebc51c857da89ce15fe
Daniel Wilson <daniel.wilson@kainos.com>
2024-09-13 12:32:53 +0100

    vm-bootsrap yellow squad xdr branch

--- extensions.tf
+++ extensions.tf
@@ -7,7 +7,7 @@ module "vm-bootstrap" {
   }
 
   count  = var.install_splunk_uf == true || var.nessus_install == true || var.run_command == true ? 1 : 0
-  source = "git::https://github.com/hmcts/terraform-module-vm-bootstrap.git?ref=master"
+  source = "git::https://github.com/hmcts/terraform-module-vm-bootstrap.git?ref=bugfix-xdr-run-command-rhel6-azcopy"
 
   virtual_machine_type       = "vm"
   virtual_machine_id         = lower(var.vm_type) == "linux" ? azurerm_linux_virtual_machine.linvm[0].id : azurerm_windows_virtual_machine.winvm[0].id
