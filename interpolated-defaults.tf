f5004affd9a5c6b52452ede01affe267196f0460
nicholas chang <nicholasc001@hotmail.com>
2024-09-23 11:52:49 +0100

    add tags for nonprod and prod

--- interpolated-defaults.tf
+++ interpolated-defaults.tf
@@ -2,12 +2,12 @@ locals {
   dynamic_boot_diagnostics = var.boot_diagnostics_enabled == true ? { dummy_create = true } : {}
   identity                 = var.systemassigned_identity == true || length(var.userassigned_identity_ids) > 0 ? { identity = { type = (var.systemassigned_identity && length(var.userassigned_identity_ids) > 0 ? "SystemAssigned, UserAssigned" : !var.systemassigned_identity && length(var.userassigned_identity_ids) > 0 ? "UserAssigned" : "SystemAssigned"), identity_ids = var.userassigned_identity_ids } } : {}
 
-  xdr_tags = join(",",
+  xdr_tags = join(",", flatten([
     "hmcts",
     "server",
     "crimeportal",
     "heritage",
-    var.env == "production" ? "production" : "nonprod",
+    [var.env == "production" ? "production" : "nonprod"],
     [for key, value in var.tags : "${key}=${value}"]
-  )
+  ]))
 }
