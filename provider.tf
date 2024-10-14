809bc04efcbea2dbca3390d8a7bf079ebfaf388c
nicholas chang <nicholasc001@hotmail.com>
2024-10-14 12:12:44 +0100

    add test branch for 4x

--- provider.tf
+++ provider.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source                = "hashicorp/azurerm"
+      version               = "4.5.0"
       configuration_aliases = [azurerm.cnp, azurerm.soc, azurerm.dcr]
     }
   }
