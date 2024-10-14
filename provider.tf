c812779666c832ae7b521e0e900398fdd034e5a3
nicholas chang <nicholasc001@hotmail.com>
2024-10-14 12:39:56 +0100

    update example code

--- provider.tf
+++ provider.tf
@@ -2,7 +2,6 @@ terraform {
   required_providers {
     azurerm = {
       source                = "hashicorp/azurerm"
-      version               = "4.5.0"
       configuration_aliases = [azurerm.cnp, azurerm.soc, azurerm.dcr]
     }
   }
