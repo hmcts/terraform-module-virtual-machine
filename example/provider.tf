43ea5b41c442cda66e63e2b65df4dcc2f6ef66ce
nicholas chang <nicholasc001@hotmail.com>
2024-10-14 12:40:40 +0100

    format code

--- example/provider.tf
+++ example/provider.tf
@@ -17,7 +17,7 @@ provider "azurerm" {
 }
 
 provider "azurerm" {
-  alias                      = "dcr"
+  alias                           = "dcr"
   resource_provider_registrations = "all"
   features {}
   subscription_id = var.env == "prod" || var.env == "production" ? "8999dec3-0104-4a27-94ee-6588559729d1" : var.env == "sbox" || var.env == "sandbox" ? "bf308a5c-0624-4334-8ff8-8dca9fd43783" : "1c4f0704-a29e-403d-b719-b90c34ef14c9"
