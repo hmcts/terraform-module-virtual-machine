0e23a1573efd88c996a2484ba441106457c6f5f2
Chirag Pareek <chirag.pareek@justice.gov.uk>
2025-05-01 12:20:46 +0100

    added system assigned identity for the VM

--- interpolated-defaults.tf
+++ interpolated-defaults.tf
@@ -1,5 +1,5 @@
 locals {
   dynamic_boot_diagnostics = var.boot_diagnostics_enabled == true ? { dummy_create = true } : {}
-  identity                 = var.systemassigned_identity == true || length(var.userassigned_identity_ids) > 0 ? { identity = { type = (var.systemassigned_identity && length(var.userassigned_identity_ids) > 0 ? "SystemAssigned, UserAssigned" : !var.systemassigned_identity && length(var.userassigned_identity_ids) > 0 ? "UserAssigned" : "SystemAssigned"), identity_ids = var.userassigned_identity_ids } } : {}
+  identity                 = var.systemassigned_identity == true || length(var.userassigned_identity_ids) > 0 ? { identity = { type = (var.systemassigned_identity && length(var.userassigned_identity_ids) > 0 ? "SystemAssigned, UserAssigned" : !var.systemassigned_identity && length(var.userassigned_identity_ids) > 0 ? "UserAssigned" : "SystemAssigned"), identity_ids = var.userassigned_identity_ids } } : var.deploy_entra_extension == true ? { identity = { type = "SystemAssigned" } } : {}
   admin_ssh_key            = var.vm_admin_ssh_key != null && var.disable_password_authentication == true ? { dummy_create = true } : {}
 }
