7967f1bdd867d1d5d03d94ced6dd605d01bf0bc7
Alex Bance <Alex.Bance@justice.gov.uk>
2025-05-19 12:40:26 +0100

    feat: support source_image_id

--- inputs-optional.tf
+++ inputs-optional.tf
@@ -449,7 +449,12 @@ variable "aad_type_handler_version" {
   description = "AADSSHLoginForLinux type handler version"
   type        = string
   default     = "1.0"
+}
 
+variable "source_image_id" {
+  description = "ID of an image to use for the VM, can be a marketplace image or a customer managed image."
+  type        = string
+  default     = null
 }
 
 variable "enable_fileshare" {
